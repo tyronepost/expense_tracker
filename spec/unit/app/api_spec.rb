da73e3e97041b951d6a5e93f36f3e3064339cb01
tyronepost <tyrone.post.a@gmail.com>
2017-12-10 09:54:56 -0600

    testing git shortcuts

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -73,6 +73,7 @@ module ExpenseTracker
           get "expenses/#{date}"
           expect(last_response.status).to eq(200)
         end
+
       end
 
       context 'when there are no expenses on the given date' do
